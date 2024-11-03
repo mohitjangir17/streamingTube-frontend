23de4633a4a261b3d247758c1814a0ad4f05adc0
Mohit <mohitjangir17@gmail.com>
2024-11-03 15:51:41 +0530

    basic env changes

--- src/main.jsx
+++ src/main.jsx
@@ -61,7 +61,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLEOAUTH_CLIENTID} >
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENTID} >
       <AuthContextProvider>
         <BrowserRouter >
           <App />
